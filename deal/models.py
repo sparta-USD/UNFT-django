36d2128b705aa05b42756f0a1a371ea7a524bf26
haeminchoi2 <sjhk02@gmail.com>
2022-11-22 21:43:27 +0900

    Feat: 거래내역 Deal 모델 생성 #5
    - 거래내역 Deal모델 작성했습니다.

--- deal/models.py
+++ deal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from unft.models import Unft
+# from users.models import User
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Deal(models.Model):
+    unft = models.ForeignKey(Unft, on_delete = models.CASCADE, related_name="unft_deals")
+    from_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name="from_user_deals")
+    to_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name="to_user_deals")
+    price = models.IntegerField(default=0)
+    status = models.IntegerField(default=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
