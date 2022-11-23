b66707968b17f611c0c4721a3663c797fbeb10cd
LeeHyunji <hyunji015@gmail.com>
2022-11-23 18:32:02 +0900

    Feat: Unft(작품)모델 상세조회 API 구현 #8
    1. Unft 모델 상세 조회
    	- UnftSerializer를 사용
    	- get_object()사용해서 id가 없는 경우 404예외처리

--- unft/views.py
+++ unft/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 from .models import Unft
-from .serializers import UnftSerializer,UnftCreateSerializer
+from .serializers import *
 from django.contrib.auth import get_user_model
 
 # Create your views here.
@@ -13,10 +14,23 @@ class UnftList(APIView):
         return Response(serializer.data)
 
     def post (self, request):
-        request_user = get_user_model().objects.get(id=1)
+        # request_user = request.user        # 로그인한 유저
+        request_user = get_user_model().objects.get(id=1)  # 로그인한 유저(임시 1번 유저)
         request.data.update({'creator': request_user.id, 'owner': request_user.id})
         serializer = UnftCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UnftDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Unft.objects.get(id=id)
+        except Unft.DoesNotExist:
+            raise Http404
+
+    def get (self, request, id):
+        target_unft = self.get_object(id)
+        serializer = UnftSerializer(target_unft)
+        return Response(serializer.data)
