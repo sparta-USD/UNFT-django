18b825ab04917a30ea77c20525cf556a178a336f
haeminchoi2 <sjhk02@gmail.com>
2022-11-23 14:06:56 +0900

    Feat: 거래내역 Serializer 수정 #5
    - 필요한 필드(구매자/판매자의 username)를 Response에 전달하기 위해 ReadOnlyField 추가했습니다.

--- deal/serializers.py
+++ deal/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from deal.models import Deal
 
+
 class DealSerializer(serializers.ModelSerializer):
+    from_user_username = serializers.ReadOnlyField(source='from_user.username')
+    to_user_username = serializers.ReadOnlyField(source='to_user.username')
     class Meta:
         model = Deal
-        fields = '__all__'
+        fields = [
+            'id',
+            'unft',
+            'from_user',
+            'from_user_username',
+            'to_user_username',
+            'price',
+            'status',
+            'created_at',
+            'updated_at'
+            ]
+        write_only_fields = ['from_user']
