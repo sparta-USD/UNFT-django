b02f66f5bb04ffa8907a7772940377ca64b3eddc
haeminchoi2 <sjhk02@gmail.com>
2022-11-23 14:49:10 +0900

    Feat: 거래내역 생성, 거래내역 전체목록 구현 완료 #5
    - url : /deal/ 에 해당하는 API구현 완료했습니다.

--- deal/views.py
+++ deal/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from deal.models import Deal
+from deal.serializers import DealSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class DealView(APIView):
+    def post(self, request):
+        serializer = DealSerializer(data=request.data)
+        if serializer.is_valid():
+            #TODO from_user_id를 body에 담아 request.data에서 꺼내올 수 있는가?
+            serializer.save(to_user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        deals = Deal.objects.all()
+        serializer = DealSerializer(deals, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
