52ee32a58fa528c373feb14a1658bd200ce3a3d5
hyunjooooojung <hyunjoo3107@gmail.com>
2022-11-23 14:10:14 +0900

    Feat : 회원가입 시 예외처리 사항 추가 #6
    - 예외처리-> ( 이름, 이메일 중복&형식 오류, 비밀번호 유효성 체크(대문자,소문자,숫자,특수문자 포함 8-20자), 비밀번호 일치 여부 확인 )
    
    Close #6

--- users/serializers.py
+++ users/serializers.py
@@ -2,11 +2,45 @@ from rest_framework import serializers
 from users.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+import re #정규식 처리 모듈
+
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+    
     class Meta:
         model = User
-        fields=['email', 'username', 'password', 'usd']
+        fields=['email', 'username', 'password','password2', 'usd']
+        
+    def validate(self, data):
+        correct_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
+        password_input = correct_password.match(data.get("password", ""))
+        password = data['password']
+        password2 = data['password2']
+
+        if data.get("username"):
+            try:
+                if User.objects.get(username=data.get("username")):
+                    raise serializers.ValidationError(
+                        detail={"username": "이미 존재하는 username입니다."}
+                    )
+            except:
+                # username의 길이가 8자가 넘을 때
+                if len(data.get("username", "")) > 8:
+                    raise serializers.ValidationError(
+                    detail={"username": "username은 8자리 이하로 설정해주세요."})
+        
+        if password_input == None:
+            raise serializers.ValidationError(
+                detail={"password": "비밀번호는 8-20자이며 최소 하나 이상의 영문자, 숫자, 특수문자가 필요합니다."})
+            
+        if password != password2:
+            raise serializers.ValidationError(
+                detail={"password": "password가 불일치합니다! 다시 확인해주세요"}
+            )
+        
+        return data
         
     def create(self, validated_data):
         user = super().create(validated_data)
