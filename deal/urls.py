5fd283d9de8bde13b4955f4617fd6554ac62e695
haeminchoi2 <sjhk02@gmail.com>
2022-11-26 18:39:02 +0900

    Feat: 거래 완료시 U-NFT owner필드 수정 #25
    - 거래내역 status=1이 되면 owner필드가 구매자로 수정됩니다.

--- deal/urls.py
+++ deal/urls.py
@@ -3,4 +3,5 @@ from deal import views
 urlpatterns = [
     path('', views.DealView.as_view(), name="deal_view"),
     path('<int:id>/', views.DealDetailView.as_view(), name="deal_detail_view"),
+    path('complete/<int:id>/', views.DealCompleteView.as_view(), name="deal_complete_view"),
 ]
